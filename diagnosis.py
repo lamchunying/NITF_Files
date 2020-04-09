86081098c6c34b3620d5924856942d0eb37e97b6
lamchunying <bellalam1388@gmail.com>
2020-04-09 13:01:11 -0700

    add diagnosis.py

---
+++ diagnosis.py
@@ -0,0 +1,12 @@
+from osgeo import gdal
+dataset = gdal.Open("checkerb.tif", gdal.GA_ReadOnly)
+print("Driver: {}/{}".format(dataset.GetDriver().ShortName,
+                            dataset.GetDriver().LongName))
+print("Size is {} x {} x {}".format(dataset.RasterXSize,
+                                    dataset.RasterYSize,
+                                    dataset.RasterCount))
+print("Projection is {}".format(dataset.GetProjection()))
+geotransform = dataset.GetGeoTransform()
+if geotransform:
+    print("Origin = ({}, {})".format(geotransform[0], geotransform[3]))
+    print("Pixel Size = ({}, {})".format(geotransform[1], geotransform[5]))
